c43a3f86836d331e0e74759ea8df70540bbebdc2
Danny Warren <dannydwarren@gmail.com>
2021-07-21 15:53:10 -0600

    Add MediaServer Environment

--- Configurator/Configurator/InstallEnvironment.cs
+++ Configurator/Configurator/InstallEnvironment.cs
@@ -7,6 +7,7 @@ namespace Configurator
     {
         Personal = 1 << 0,
         Work = 1 << 1,
-        All = Personal | Work
+        MediaServer = 1 << 2,
+        All = Personal | Work | MediaServer
     }
 }
