c4129f484883627fd693925358406b6d72b0ad65
Danny Warren <dannydwarren@gmail.com>
2021-09-13 21:52:57 -0600

    Switch to System.CommandLine
    drop System.CommandLineDragonFruit
    Set default version: 0.0.0-dev

--- Configurator/Configurator/Program.cs
+++ Configurator/Configurator/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.Linq;
+using System.Reflection;
 using Configurator.Configuration;
 using Configurator.Utilities;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,22 +15,59 @@ namespace Configurator
     class Program
     {
         // manifestPath: @"https://raw.githubusercontent.com/dannydwarren/machine-configs/main/manifests/danny.manifest.json",
-        static async Task Main(string manifestPath = @"https://raw.githubusercontent.com/dannydwarren/machine-configs/main/manifests/manifest_test.json", string environments = "Test")
+        static async Task<int> Main(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+                new Option<string>(
+                    aliases: new[] { "--manifest-path", "-m" },
+                    getDefaultValue: () =>
+                        "https://raw.githubusercontent.com/dannydwarren/machine-configs/main/manifests/manifest_test.json",
+                    description: "Path (local or URL) to your manifest file."),
+                new Option<List<string>>(
+                    aliases: new[] { "--environments", "-e" },
+                    parseArgument: x =>
+                        x.Tokens.FirstOrDefault()?.Value.Split("|", StringSplitOptions.RemoveEmptyEntries).ToList()
+                        ?? new List<string> { "Test" },
+                    isDefault: true,
+                    description: "Pipe separated list of environments to target in the manifest.")
+            };
+
+            rootCommand.Description = "Configurator";
+
+            rootCommand.Handler = CommandHandler.Create<string, List<string>>(Run);
+
+            return await rootCommand.InvokeAsync(args);
+        }
+
+        private static async Task Run(string manifestPath, List<string> environments)
         {
             var services = ConfigureServices(new Arguments(
                 manifestPath: manifestPath,
-                environments: environments.Split("|", StringSplitOptions.RemoveEmptyEntries).ToList()
+                environments: environments
             ));
 
-            var powerShell = services.GetRequiredService<IPowerShell>();
-            var result = await powerShell.ExecuteAsync("$PSVersionTable.PSVersion.ToString()");
-            Console.WriteLine($"PowerShell Version: {result.AsString}");
+            await WriteInitialDebugInfo(services);
 
             var config = services.GetRequiredService<IMachineConfigurator>();
 
             await config.ExecuteAsync();
 
             await services.DisposeAsync();
+        }
+
+        private static async Task WriteInitialDebugInfo(ServiceProvider services)
+        {
+            var logger = services.GetRequiredService<IConsoleLogger>();
+
+            var version = (Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly())
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!
+                .InformationalVersion;
+            logger.Debug($"{nameof(Configurator)} version: {version}");
+
+            var powerShell = services.GetRequiredService<IPowerShell>();
+            var result = await powerShell.ExecuteAsync("$PSVersionTable.PSVersion.ToString()");
+            logger.Debug($"PowerShell Version: {result.AsString}");
         }
 
         private static ServiceProvider ConfigureServices(Arguments arguments)
