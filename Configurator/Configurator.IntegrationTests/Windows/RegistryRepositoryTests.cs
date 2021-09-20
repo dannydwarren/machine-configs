41a1b08a408dff6690ba92320f768e5ff673c8ca
Danny Warren <dannydwarren@gmail.com>
2021-09-19 21:12:27 -0600

    Add RegistryRepository

---
+++ Configurator/Configurator.IntegrationTests/Windows/RegistryRepositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Configurator.Windows;
+using Shouldly;
+using Xunit;
+
+namespace Configurator.IntegrationTests.Windows
+{
+    public class RegistryRepositoryTests : IntegrationTestBase<RegistryRepository>
+    {
+        [Fact]
+        public void When_getting_the_value_of_a_registry_key()
+        {
+            var value = Because(() => ClassUnderTest.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion", "ProgramFilesDir (x86)"));
+
+            It("retrieves the value", () =>
+            {
+                value.ShouldBe(@"C:\Program Files (x86)");
+            });
+        }
+
+        [Fact]
+        public void When_setting_the_value_of_a_registry_key()
+        {
+            var keyName = $@"HKEY_CURRENT_USER\SOFTWARE\{nameof(Configurator)}.{nameof(RegistryRepositoryTests)}";
+            var valueName = nameof(When_setting_the_value_of_a_registry_key);
+            var expectedValue = $"Time of test: {DateTimeOffset.Now:yyyy-MM-dd hh:mm:ss:fffff}";
+
+            Because(() => ClassUnderTest.SetValue(keyName, valueName, expectedValue));
+
+            var actualValue = ClassUnderTest.GetValue(keyName, valueName);
+
+            It("retrieves the value", () =>
+            {
+                actualValue.ShouldBe(expectedValue);
+            });
+        }
+    }
+}
---
+++ Configurator/Configurator/Windows/RegistryRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Win32;
+
+namespace Configurator.Windows
+{
+    public interface IRegistryRepository
+    {
+        string GetValue(string keyName, string valueName);
+        void SetValue(string keyName, string valueName, object value);
+    }
+
+    public class RegistryRepository : IRegistryRepository
+    {
+        public string GetValue(string keyName, string valueName)
+        {
+            return Registry.GetValue(keyName, valueName, "").ToString()!;
+        }
+
+        public void SetValue(string keyName, string valueName, object value)
+        {
+            Registry.SetValue(keyName, valueName, value);
+        }
+    }
+}
