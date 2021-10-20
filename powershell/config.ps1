ccdc0bf8fb9e8f5cb0c31984dde607c885845b4c
Danny Warren <dannydwarren@gmail.com>
2021-10-19 22:21:23 -0600

    Add oh-my-posh and BurntToast to manifest

--- powershell/config.ps1
+++ powershell/config.ps1
@@ -1,22 +1,3 @@
-Block "Install posh-git" {
-    Install-Module posh-git -Force
-    Add-PoshGitToProfile -AllHosts
-} {
-    Get-Module -ListAvailable posh-git
-}
-
-Block "Install oh-my-posh" {
-    Install-Module oh-my-posh -Scope CurrentUser -AllowPrerelease -Force
-} {
-    Get-Module -ListAvailable oh-my-posh
-}
-
-Block "Install BurntToast" {
-    Install-Module BurntToast -Force
-} {
-    Get-Module -ListAvailable BurntToast
-}
-
 Block "PowerShell Transcripts" {
     mkdir "$backupDir\PowerShell Transcripts" -ErrorAction Ignore
 }
@@ -26,16 +7,6 @@ Block "Configure profile.ps1" {
 } {
     (Test-Path $profile) -and (Select-String "$($PSScriptRoot -replace "\\", "\\")\\profile.ps1" $profile) # <original> is regex, <substitute> is PS string
 }
-
-Block "Configure scoop nerd-fonts bucket" {
-    scoop bucket add nerd-fonts
-} {
-    scoop bucket list | Select-String nerd-fonts
-}
-
-InstallFromScoopBlock "Cascadia Code" CascadiaCode-NF
-
-InstallFromMicrosoftStoreBlock "Windows Terminal" 9n0dx20hk701 Microsoft.WindowsTerminal
 
 if (!(Configured $forTest)) {
     FirstRunBlock "Update PS help" {
