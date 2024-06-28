f29299b588208b8bcb72f5f1107918a26b440fc7
Danny Warren <dannydwarren@gmail.com>
2024-06-28 13:35:54 -0600

    Fix Danny PowerShell Profile quoting bug

--- apps/Danny PowerShell Profile/install.ps1
+++ apps/Danny PowerShell Profile/install.ps1
@@ -1,2 +1,2 @@
 New-Item -Path $profile -ItemType File -ErrorAction Ignore
-Add-Content -Path $profile -Value "`n. $($PSScriptRoot)\danny.profile.ps1"
+Add-Content -Path $profile -Value "`n. '$($PSScriptRoot)\danny.profile.ps1'"
