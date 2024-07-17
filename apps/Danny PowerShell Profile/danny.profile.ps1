686bea1f036765c7deaf6446850de1b85a9ba735
Danny Warren <dannydwarren@gmail.com>
2024-07-17 10:08:43 -0600

    Format tabs of danny.profile.ps1

--- apps/Danny PowerShell Profile/danny.profile.ps1
+++ apps/Danny PowerShell Profile/danny.profile.ps1
@@ -21,20 +21,20 @@ mkdir $transcripts -ErrorAction Ignore
 $InformationPreference = 'Continue'
 
 function setLocationToSrc() {
-    if (($Pwd -like '*system32*') -or ($Pwd -like '*danny*')) {
-        Set-Location $src
-    }
+  if (($Pwd -like '*system32*') -or ($Pwd -like '*danny*')) {
+    Set-Location $src
+  }
 }
 setLocationToSrc
 
 function applyTheme() {
-    $env:POSH_GIT_ENABLED = $true
-    oh-my-posh init pwsh --config c:\src\machine-configs\powershell\ben.omp.json | Invoke-Expression
-    Enable-PoshLineError
+  $env:POSH_GIT_ENABLED = $true
+  oh-my-posh init pwsh --config c:\src\machine-configs\powershell\ben.omp.json | Invoke-Expression
+  Enable-PoshLineError
 }
 
 if ($PSVersionTable.PSVersion.Major -gt 5) {
-    applyTheme
+  applyTheme
 }
 
 Set-Alias -Name tf -Value terraform.exe
@@ -42,29 +42,32 @@ Set-Alias -Name android -Value scrcpy
 
 # TODO: Run this as nightly job
 function clear-clipboard {
-    Restart-Service -Name "cbdhsvc*" -force
+  Restart-Service -Name "cbdhsvc*" -force
 }
 
 function clone-repo($repositoryOrganization, $repositoryName) {
-    if (![System.IO.File]::Exists("$src\$repositoryName")) {
-        git clone "https://github.com/$repositoryOrganization/$repositoryName.git"
-    }
+  if (![System.IO.File]::Exists("$src\$repositoryName")) {
+    git clone "https://github.com/$repositoryOrganization/$repositoryName.git"
+  }
 }
 
 #OPTIONS: dev-phoenix
 function setAwsProfile($awsProfile) {
-    aws s3 ls --profile $awsProfile
-    setJnLocalEnv $awsProfile
+  aws s3 ls --profile $awsProfile
+  setJnLocalEnv $awsProfile
 }
 function loginAws($awsProfile) {
-    aws sso login --profile $awsProfile
-    setJnLocalEnv $awsProfile
+  aws sso login --profile $awsProfile
+  setJnLocalEnv $awsProfile
 }
-function setJnLocalEnv($awsProfile){
-    $env:AWS_PROFILE = $awsProfile
-    $env:AWS_REGION = (aws configure get region --profile $env:AWS_PROFILE)
+function setJnLocalEnv($awsProfile) {
+  $env:AWS_PROFILE = $awsProfile
+  $env:AWS_REGION = (aws configure get region --profile $env:AWS_PROFILE)
 }
-function generateApi2Env(){
-    jnlocal dotenv --source taskdef webapp-api2-dev -o "$src\webappnew\future\api\.env" -w "$HOME\JobNimbus\local-overrides.json"
+function generateLocalEnv() {
+  #to find something aws ecs list-task-definitions | select-string "the thing"
+  $overridePath = "$HOME\JobNimbus\local-overrides.json"
+  jnlocal dotenv --source taskdef webapp-api2-dev -o "$src\webappnew\future\api\.env" -w $overridePath
+  jnlocal dotenv --source taskdef jncore-nodeapi-dev -o "$src\webappnew\NodeServer\.env" -w $overridePath
 }
 
