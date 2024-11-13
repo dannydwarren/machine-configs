37e7f3cf7e15256d720742dff7557c39e4b4040e
Danny Warren <danny.warren@jobnimbus.com>
2024-11-13 10:07:13 -0700

    Add JN lionAutomation repo and  'Remove Dell Bloatware'

---
+++ apps/Remove Dell Bloatware/install.ps1
@@ -0,0 +1,5 @@
+winget uninstall --name "Dell Digital Delivery"
+winget uninstall --name "Dell SupportAssist for Home PCs"
+
+# NOTE: Does not run silently
+winget uninstall --id "ARP\Machine\X64\Dell Pair"
