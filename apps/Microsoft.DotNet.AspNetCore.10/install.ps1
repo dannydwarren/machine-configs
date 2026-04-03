bb47e13c8f58b842e81bd81e12c6b761b1a33a0c
Danny Warren <danny.warren@jobnimbus.com>
2026-04-03 13:57:29 -0600

    Add .NET 10 Runtime and ASP.NET Core Runtime (x64)

---
+++ apps/Microsoft.DotNet.AspNetCore.10/install.ps1
@@ -0,0 +1,1 @@
+winget install --id Microsoft.DotNet.AspNetCore.10 --architecture x64 --accept-package-agreements -h -e
---
+++ apps/Microsoft.DotNet.Runtime.10/install.ps1
@@ -0,0 +1,1 @@
+winget install --id Microsoft.DotNet.Runtime.10 --architecture x64 --accept-package-agreements -h -e
